cf93af676a055df2637cce7ccaf918056ddf5516
Minh Triet <minhtriet@maytinhdian.com>
2025-08-23 05:16:17 +0700

    Refactor Pluggin

--- src/Domain/Repositories/DebtRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-// src/Domain/Repositories/debt-repository-interface.php
-namespace TMT\CRM\Domain\Repositories;
-
-interface DebtRepositoryInterface {
-    public function mark_paid(int $debt_id): bool;
-}
--- src/Domain/Repositories/PaymentRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-// src/Domain/Repositories/payment-repository-interface.php
-namespace TMT\CRM\Domain\Repositories;
-
-interface PaymentRepositoryInterface
-{
-    public function create(int $invoice_id, float $amount, ?string $note = null): int;
-    public function find_by_invoice(int $invoice_id): array; // list bản ghi
-}
